ed8f7db39fc74171abae6782ad66dc94827dee27
Bernardo de Freitas Zamperetti <bfzamperetti@gmail.com>
2016-04-06 23:22:02 +0200

    header politician and logo

--- client/app/src/configs/material.js
+++ client/app/src/configs/material.js
@@ -3,7 +3,7 @@ app.config(function($mdThemingProvider, $mdIconProvider){
    .defaultIconSet("./assets/svg/avatars.svg", 128)
    .icon("menu"       , "./assets/svg/menu.svg"        , 24)
    .icon("share"      , "./assets/svg/share.svg"       , 24)
-   .icon("logo"      ,  "./assets/svg/logo.svg"       , 512)
+   .icon("logo"       , "./assets/svg/logo.svg")
    .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
    .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
    .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
