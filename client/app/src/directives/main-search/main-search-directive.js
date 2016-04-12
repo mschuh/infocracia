49adae5d6c12d6b8ebf78cf6d861fe8555bd7713
Bernardo de Freitas Zamperetti <bfzamperetti@gmail.com>
2016-04-12 20:51:54 +0200

    home more professional and responsive

--- client/app/src/directives/main-search/main-search-directive.js
+++ client/app/src/directives/main-search/main-search-directive.js
@@ -1,25 +1,24 @@
-app.directive('plMainSearch', function() {
+app.directive('plMainSearch', ["$q", "$timeout", function($q, $timeout) {
   return {
     link: function(scope, element, attrs, controllers) {
-      scope.search = {
-	      searchEntityText: ""
-	      
-	  };
-
-
+	scope.searchEntityText = "";
+	
 	var data = [
 		{ name: "Dilma Roussef", photoUrl:"http://cenariotocantins.com.br/principal/wp-content/uploads/2012/09/presidente-dilma-rousseff-pesquisa-aumenta-popularidade-da-presidente-brasil.jpg"},
 		{ name: "Jair Bolsonaro", photoUrl: "http://curitibainenglish.com.br/wp-content/uploads/2014/12/jair-bolsonaro-racista.jpg"}
 	    ];
 
-	scope.search.querySearchEntity = function(param){
-	       
-	    return data.filter(function(entity){
-		return entity.name.indexOf(param) >= 0;
-	    })
-	    
+	scope.querySearchEntity = function(query) {
+	    var deferred = $q.defer();
+	    $timeout(function () { 
+		var res = data.filter(function(entity){ 
+		    return entity.name.indexOf(query) >= 0; 
+		});
+		deferred.resolve(res); 
+	    }, Math.random() * 1000, false);
+	    return deferred.promise;
 	}
     },
     templateUrl: './src/directives/main-search/main-search-directive.html'
   };
-});
+}]);
